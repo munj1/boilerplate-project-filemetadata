126e110c465971304471f7e8cc7e8c87cb45a744
munj1 <yimunj931@gmail.com>
2022-10-20 02:08:02 +0900

    multer

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+// body parser
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// multer package
+var multer = require("multer");
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", function (req, res) {
+  // const { upfile } = req.body;
+  // get metadata from file (name, type, size) using multer
+  const storage = multer.memoryStorage();
+  const upload = multer({ storage }).single("upfile");
+  upload(req, res, function (err) {
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+    const { originalname, mimetype, size } = req.file;
+    res.json({ name: originalname, type: mimetype, size: size });
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
